01d740bca84c0b5045e18bb147523ecafb062cb8
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2018-12-12 19:20:57 -0500

    Remove example debug output

--- day7/solution_pt2.rb
+++ day7/solution_pt2.rb
@@ -27,20 +27,14 @@ Step F must be finished before step E can begin.
   until big_hash.empty?
     remaining_keys = big_hash.keys.sort
     current_steps = remaining_keys.select { |k| big_hash[k].length.zero? }.first(available_workers)
-    puts "Current Steps:::::"
-    puts current_steps
     available_workers -= current_steps.length
-    puts "available_workers: #{available_workers}"
     current_steps.each do |step|
       next if active_jobs.index { |j| j[:job] == step }
       active_jobs.push({ :job => step, :finish => time_elapsed + ( step.ord - 64 + time_per_step) })
     end
-    puts "active_jobs:::::"
 
     # jump time up to finish of next job(s)
     active_jobs.sort_by! { |j| j[:finish] }
-    puts active_jobs
-    gets
     time_elapsed = active_jobs.first[:finish]
 
     finished_jobs = active_jobs.select { |j| j[:finish] <= time_elapsed }
