1524709910b105d5f1c38a09da98bade6aaeef66
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2018-12-12 19:57:04 -0500

    Complete part 2

--- day7/solution_pt2.rb
+++ day7/solution_pt2.rb
@@ -1,19 +1,8 @@
 if $PROGRAM_NAME == __FILE__
-
-  input = %(
-Step C must be finished before step A can begin.
-Step C must be finished before step F can begin.
-Step A must be finished before step B can begin.
-Step A must be finished before step D can begin.
-Step B must be finished before step E can begin.
-Step D must be finished before step E can begin.
-Step F must be finished before step E can begin.
-  )
-  # input = File.readlines('input.txt')
+  input = File.readlines('input.txt')
   big_hash = Hash.new { |hash, key| hash[key] = [] }
 
-  input.strip.split("\n").each do |l|
-  # input.each do |l|
+  input.each do |l|
     label = /step (.) can begin./.match(l)[1]
     prereq = /^Step (.) must/.match(l)[1]
     big_hash[label] << prereq
@@ -21,15 +10,17 @@ Step F must be finished before step E can begin.
   end
 
   time_elapsed = 0
-  available_workers = 2
-  time_per_step = 0
+  available_workers = 5
+  time_per_step = 60
   active_jobs = []
+  answer = ""
   until big_hash.empty?
     remaining_keys = big_hash.keys.sort
     current_steps = remaining_keys.select { |k| big_hash[k].length.zero? }.first(available_workers)
-    available_workers -= current_steps.length
     current_steps.each do |step|
       next if active_jobs.index { |j| j[:job] == step }
+      break if active_jobs.length > available_workers
+
       active_jobs.push({ :job => step, :finish => time_elapsed + ( step.ord - 64 + time_per_step) })
     end
 
@@ -39,11 +30,11 @@ Step F must be finished before step E can begin.
 
     finished_jobs = active_jobs.select { |j| j[:finish] <= time_elapsed }
     finished_jobs.each do |j|
+      answer += j[:job]
       big_hash.delete(j[:job])
       big_hash.each { |k, _| big_hash[k] -= [j[:job]] }
     end
     active_jobs -= finished_jobs
-    available_workers += finished_jobs.length
   end
-  puts time_elapsed
+  puts "Final Answer: #{time_elapsed}"
 end
